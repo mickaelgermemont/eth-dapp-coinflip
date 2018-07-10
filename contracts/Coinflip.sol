5c6fc5cd45cbe4d6d26c9df77556dab9349c1a01
Mickael GERMEMONT <mickaelgermemont@gmail.com>
2018-07-10 00:23:12 -0400

    added my changes

--- contracts/Coinflip.sol
+++ contracts/Coinflip.sol
@@ -1,30 +1,54 @@
-pragma solidity ^0.4.17;
-
-// contract will keep 5%
-// 
+pragma solidity ^0.4.22;
 
 contract Coinflip {
     address public manager;
-    address[] public players;
+    address[] public playersHeads;
+    address[] public playersTails;
 
-    function Lottery() public {
+    constructor () public {
         manager = msg.sender;
     }
 
-    function enter() public payable {
+    function enter(bool heads) public payable {
         require(msg.value > .01 ether);
-
-        players.push(msg.sender);
+        if(heads){
+            playersHeads.push(msg.sender);
+        } else {
+            playersTails.push(msg.sender);
+        }
     }
 
-    function random() private view returns (uint) {
-        return uint(keccak256(block.difficulty, now, players));
+    function computeReward(uint numberOfWinners, uint _balance) private pure returns (uint) {
+        // balance is a wei amount
+        uint reward = uint(_balance / numberOfWinners);
+        return reward;
+    }
+
+    function randomHeadsOrTails() private view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.difficulty, now, playersHeads.length, playersTails.length))) % 2;
     }
 
     function pickWinner() public restricted {
-        uint index = random() % players.length;
-        players[index].transfer(this.balance);
-        players = new address[](0);
+        require( (playersHeads.length + playersTails.length) > 0 );
+        
+        bool heads = randomHeadsOrTails() == 0;
+
+        address[] memory winners;
+        if(heads){
+            winners = playersHeads;
+        } else {
+            winners = playersTails;
+        }
+
+        uint reward = computeReward(winners.length, address(this).balance);
+
+        for (uint i=0; i<winners.length; i++) {
+            winners[i].transfer(reward);
+        }
+
+        // reset
+        playersHeads = new address[](0);
+        playersTails = new address[](0);
     }
 
     modifier restricted() {
@@ -32,7 +56,15 @@ contract Coinflip {
         _;
     }
 
-    function getPlayers() public view returns (address[]) {
-        return players;
+    function getPlayersTails() public restricted view returns (address[]) {
+        return playersTails;
+    }
+
+    function getPlayersHeads() public restricted view returns (address[]) {
+        return playersHeads;
+    }
+
+    function getRewardStack() public view returns (uint) {
+        return address(this).balance;
     }
 }
--- contracts/CoinflipSolo.sol
+++ contracts/CoinflipSolo.sol
@@ -1,12 +1,3 @@
-# eth-dapp-coinflip
-
-* define a schedule when the contract will flip the coin. every hour or based on block number. every 30bocks (30 * 20sec = 600sec = roughly 5min)
-* enter the coinflip drawing - each player decides HEADS or TAILS
-* player pays some fee to enter, there is a minimum and no maximum
-* player stake in the game determines how much he can win (winning ETH proportional to ETH stake) 
-
-* send all my funds to Contract 0x3d63f568d937d7a40be9da2b784f2f72d4cd555b  i kept losing
-
 pragma solidity ^0.4.22;
 
 contract Coinflip {
@@ -43,7 +34,7 @@ contract Coinflip {
         return address(this).balance;
     }
     
-    function() payable {
+    function() public payable {
         // i accept free money you send my way
     }
 }
